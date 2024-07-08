d6b393f27f7cf95d39b6299cf24fe2ccd5b19ab2
felipedev418 <storm712418@gmail.com>
2024-07-08 08:47:15 -0400

    feat: support/ticket feature , model, route, controller

--- controllers/supportController.js
+++ controllers/supportController.js
@@ -1,6 +1,7 @@
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 
 const FeedBack = require("../models/feedbackModel");
+const Ticket = require("../models/ticketModel");
 
 exports.createFeedback = catchAsyncErrors(async (req, res, next) => {
     const {id} = req.user;
@@ -14,6 +15,25 @@ exports.createFeedback = catchAsyncErrors(async (req, res, next) => {
     const newFeedBack = new FeedBack(feedback);
     try{
         const result = newFeedBack.save();
+        res.json({result, message: "success to create feedback"});
+    } catch (error) {
+        console.log("Error is",error);
+    }
+});
+
+exports.createTicket = catchAsyncErrors(async (req, res, next) => {
+    const {id} = req.user;
+    const extension = req.file.mimetype.split("/")[1];
+    const uploadedfilename = req.file.path + "." + extension;
+    const ticket = {
+        userId: id,
+        subject: req.body.subject,
+        description: req.body.description,
+        uploadedUrl: uploadedfilename
+    };
+    const newTicket = new Ticket(ticket);
+    try{
+        const result = newTicket.save();
         res.json({result, message: "success to create feedback"});
     } catch (error) {
         console.log("Error is",error);
--- routes/supportRoutes.js
+++ routes/supportRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { isAuthenticatedUser } = require("../middlewares/auth");
 const {
     createFeedback,
+    createTicket
 } = require("../controllers/supportController");
 const multer = require("multer");
 const upload = multer({dest: 'uploads/'});
@@ -11,5 +12,6 @@ const router = express.Router();
 
 
 router.route("/feedback").post(isAuthenticatedUser, upload.single('files') ,createFeedback);
+router.route("/ticket").post(isAuthenticatedUser, upload.single('files') ,createTicket);
 
 module.exports = router;
