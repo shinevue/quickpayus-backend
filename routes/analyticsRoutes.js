0327f7f117d3529158c35a7701b17fdd34314ea2
hy-hydra0 <hy.hydra@outlook.com>
2024-07-08 17:46:22 -0700

    Feat: add route for getting balance

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const { isAuthenticatedUser, authorizeRole } = require("../middlewares/auth");
 
-const { counts } = require("../controllers/analyticsController");
+const {
+  counts,
+  getBalanceInformation,
+} = require("../controllers/analyticsController");
 const router = express.Router();
 
 router.route("/counts").get(isAuthenticatedUser, counts);
+
+/**
+ * to get information for chart in dashbaord
+ * @param balanceframe [profits, credits, rewards]
+ * @param timeframe [day, week, month]
+ *
+ */
+router
+  .route("/:balanceframe/:timeframe")
+  .get(isAuthenticatedUser, getBalanceInformation);
 
 module.exports = router;
