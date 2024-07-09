a0e2d5e8a2a900cf8c6568b65594bcf8a668ce07
hy-hydra0 <hy.hydra@outlook.com>
2024-07-08 18:13:19 -0700

    feat:change and add analytics function

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -1,99 +1,123 @@
-const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const moment = require("moment");
+const { ObjectId } = require("mongodb");
+
 const User = require("../models/userModel");
 const HELPER = require("../helpers/index");
-const { ObjectId } = require("mongodb");
-const programCtlr = require("../controllers/programController");
-const referralCtlr = require("../controllers/referralsController");
+
+const { STATUS, ANALYTICS_TYPE } = require("../config/constants");
+
+const programCtlr = require("./programController");
+const referralCtlr = require("./referralsController");
 const depositCtlr = require("./transactionController");
-const { STATUS } = require("../config/constants");
+
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 
 exports.counts = catchAsyncErrors(async (req, res, next) => {
   const userId = new ObjectId(req.user.id) || null;
-
-  const defaultResponse = {
-    accountBalance: 0,
-    principleBalance: 0,
-    profitBalance: 0,
-    equityBalance: 0,
-    creditBalance: 0,
-    rankRewardBalance: 0,
-  };
 
   if (!userId) {
     return res.json(defaultResponse);
   }
 
-  const user = await User.findOne({ _id: userId });
-
-  if (!user) {
-    return res.json(defaultResponse);
-  }
-
-  const program = await programCtlr.findOne({
-    level: user?.investmentLevel,
-  });
-
-  const depositQuery = {
-    userId,
-    status: STATUS.PENDING,
+  const defaultResponse = {
+    accountBalance: 0,
+    profitBalance: 0,
+    depositBalance: 0,
+    equityBalance: 0,
+    creditBalance: 0,
+    rankRewardBalance: 0,
   };
 
-  const transactions = await depositCtlr.find(depositQuery);
+  defaultResponse.depositBalance = await depositCtlr.userDepositlBalanceByQuery(
+    userId
+  );
 
-  if (transactions?.length) {
-    defaultResponse.principleBalance =
-      transactions?.reduce((total, { amount }) => {
-        return total + amount;
-      }, 0) ?? 0;
-  }
+  defaultResponse.profitBalance = await depositCtlr.userProfitBalanceByQuery(
+    userId
+  );
 
-  const referrals =
-    (await referralCtlr.getAllReferrals(
-      { referralId: userId, isActive: true },
-      8
-    )) || [];
+  defaultResponse.creditBalance = await depositCtlr.userCreditBalanceByQuery(
+    userId
+  );
 
-  for (const referral of referrals) {
-    const query = { referralId: referral?._id, isActive: true };
-    const directReferralsCount =
-      (await referralCtlr.directReferralsCount(query)) ?? 0;
+  defaultResponse.equityBalance = await depositCtlr.userEquityBalanceByQuery(
+    userId
+  );
 
-    const depositQuery = {
-      userId: referral?._id,
-      status: STATUS.PENDING,
-    };
+  defaultResponse.accountBalance = await depositCtlr.userAccountBalanceByQuery(
+    userId
+  );
+  defaultResponse.rankRewardBalance =
+    await depositCtlr.userRewardBalanceByQuery(userId);
 
-    const transactions = await depositCtlr.find(depositQuery);
-
-    if (!transactions?.length) continue;
-
-    const sumOfAmount = transactions?.reduce((total, { amount }) => {
-      return total + amount;
-    }, 0);
-
-    const subProgram = program?.data?.find((row) => {
-      if (referral?.sublevel == row?.level) {
-        //need to add more checks
-        return row;
-      }
-    });
-    if (!subProgram) continue;
-
-    console.log(
-      referral?.username,
-      directReferralsCount,
-      subProgram?.level,
-      Number(subProgram?.creditPercentage),
-      sumOfAmount
-    );
-
-    const appliedPercentage = HELPER.applyPercentage(
-      sumOfAmount,
-      Number(subProgram?.creditPercentage)
-    );
-
-    defaultResponse.creditBalance += appliedPercentage;
-  }
-
-  return res.json(defaultResponse);
+  res.json(defaultResponse);
 });
+
+exports.getBalanceInformation = catchAsyncErrors(async (req, res, next) => {
+  const { id: userid } = req.user;
+  const { balanceframe, timeframe } = req.params;
+
+  const balanceInformation = [];
+
+  const dateQueries = generateDateQuery(timeframe);
+
+  let userBalanceFunction;
+  switch (balanceframe) {
+    case ANALYTICS_TYPE.CREDIT:
+      userBalanceFunction = depositCtlr.userCreditBalanceByQuery;
+      break;
+    case ANALYTICS_TYPE.PROFIT:
+      userBalanceFunction = depositCtlr.userProfitBalanceByQuery;
+      break;
+    case ANALYTICS_TYPE.REWARD:
+      userBalanceFunction = depositCtlr.userRewardBalanceByQuery;
+      break;
+  }
+
+  for (let i = 0; i < dateQueries.length; i++) {
+    balanceInformation.push(
+      await userBalanceFunction(userid, {
+        createdAt: dateQueries[i],
+      })
+    );
+  }
+
+  res.status(200).json(balanceInformation);
+});
+
+function generateDateQuery(timeframe) {
+  let dateQueries = [];
+
+  let start, end, last, unit;
+
+  switch (timeframe) {
+    case "day":
+      unit = "days";
+      start = moment().startOf("month");
+      last = moment().endOf("month");
+      break;
+    case "week":
+      unit = "weeks";
+      start = moment().startOf("month");
+      last = moment().endOf("month");
+      break;
+    case "month":
+      unit = "months";
+      start = moment().startOf("year");
+      last = moment().endOf("year");
+      break;
+    default:
+      throw new Error("Invalid date type");
+  }
+
+  while (start < last) {
+    end = moment(start).add(1, unit);
+    dateQueries.push({
+      $gte: start.toDate(),
+      $lte: end.toDate(),
+    });
+    start = end;
+  }
+
+  return dateQueries;
+}
