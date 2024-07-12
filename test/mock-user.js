4210341880cef476b73868966013b362c184ec21
hy-hydra0 <hy.hydra@outlook.com>
2024-07-12 14:04:47 +0900

    fix : update user seed func

--- test/mock-user.js
+++ test/mock-user.js
@@ -1,58 +1,62 @@
 const connectDB = require("../config/db.js");
 const User = require("../models/userModel.js");
-const HELPER = require('../helpers');
-const { fakerEN: faker } = require('@faker-js/faker');
+const HELPER = require("../helpers");
+const { fakerEN: faker } = require("@faker-js/faker");
 
-const ProfitConfig = require('../models/profitConfigModel.js')
+const ProfitConfig = require("../models/profitConfigModel.js");
 
 connectDB();
 
 async function insertUsers(num) {
-    try {
-        for (let i = 0; i < num; i++) {
-            const newUser = new User({
-                uuid: HELPER.randomUUID(),
-                firstName: faker.person.firstName(),
-                lastName: faker.person.lastName(),
-                username: faker.internet.userName() + i,
-                email: `${faker.internet.userName()}.${i}@mail.com`,
-                countryCode: '1',
-                phoneNumber: '12340000' + ('0000' + i).slice(-4),
-                referralId: null,
-                investmentLevel: 'A',
-                investmentSubLevel: 'A2',
-                password: '123456',
-                termsAndConditions: true,
+  try {
+    for (let i = 0; i < num; i++) {
+      const newUser = new User({
+        uuid: HELPER.randomUUID(),
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        username: faker.internet.userName() + i,
+        email: `${faker.internet.userName()}.${i}@mail.com`,
+        countryCode: "1",
+        phoneNumber: "12340000" + ("0000" + i).slice(-4),
+        referralId: null,
+        investmentLevel: "A",
+        investmentSubLevel: "A2",
+        password: "123456",
+        termsAndConditions: true,
 
-                depositBalance: 100,
-                rewardBalance: 0,
-                alertNotifications: true,
-                emailNotifications: true
-            });
-            await newUser.save();
-        }
-        console.log('Done 5000 users!')
-
-        // const profit = new ProfitConfig({ profit: [50, 100, 150, 200] });
-        // profit.save();
-    } catch (e) {
-        console.error('----------Error inserting users:', e);
+        depositBalance: 100,
+        rewardBalance: 0,
+        alertNotifications: true,
+        emailNotifications: true,
+      });
+      await newUser.save();
     }
-}
+    console.log(`Done ${num} users!`);
 
+    // const profit = new ProfitConfig({ profit: [50, 100, 150, 200] });
+    // profit.save();
+  } catch (e) {
+    console.error("----------Error inserting users:", e);
+  }
+}
 
 async function updateUserReferrals() {
-    try {
-        let firstUser = await User.find({}).limit(1);
-        let users = User.find({}).skip(1).limit(50);
-        (await users).forEach(user => {
-            user.referralId = firstUser[0]._id
-            user.save();
-        })
-    } catch (e) {
-        console.error('----------Error updating users:', e);
-    }
+  try {
+    let firstUser = await User.find({}).limit(10);
+    let users = await User.find({}).skip(10).limit(3000);
+    users.forEach(async (user, index) => {
+      user.referralId = firstUser[Math.floor(Math.random() * 10)]._id;
+      await user.save();
+      console.log(`------------------Done ${index} user--------------`);
+    });
+  } catch (e) {
+    console.error("----------Error updating users:", e);
+  }
 }
 
-insertUsers(10);
-updateUserReferrals();
+async function seedUser() {
+  //   await insertUsers(5000);
+  await updateUserReferrals();
+}
+
+seedUser();
