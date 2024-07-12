7a8b23b8b96a09bead8463c34b4e734c6d52162a
hy-hydra0 <hy.hydra@outlook.com>
2024-07-12 14:19:33 +0900

    fix: update claimReward func

--- controllers/rewardsController.js
+++ controllers/rewardsController.js
@@ -2,7 +2,9 @@ const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const ErrorHandler = require("../utils/errorHandler");
 const Reward = require("../models/rewardModel");
 const { STATUS } = require("../config/constants");
-
+const Rank = require("../models/rankModel");
+const RankCtrl = require("../controllers/ranksController");
+const { ObjectId } = require("mongodb");
 exports.get = catchAsyncErrors(async (req, res, next) => {
   const { type, page = 1 } = req?.query || {};
   const userId = req.user.id;
@@ -28,26 +30,38 @@ exports.get = catchAsyncErrors(async (req, res, next) => {
 });
 
 exports.claimReward = catchAsyncErrors(async (req, res, next) => {
-  const userId = req?.user?.id;
-  const { rankId } = req?.body || {},
-    status = STATUS.PENDING;
+  const lastReward = await Reward.findOne().sort({ _id: -1 });
 
-  const reward = await Reward.findOne({ userId, rankId, status });
-
-  if (reward) {
-    return next(
-      new ErrorHandler(`Claimed reward is already in ${status} status!`)
-    );
-  } else {
-    const newReward = new Reward({
-      userId,
-      rankId,
-    });
-
-    await newReward.save();
+  if (lastReward && lastReward.status == STATUS.PENDING) {
+    next(new ErrorHandler("Alearyd Claimed", 400));
   }
+  const userId = new ObjectId(req.user.id);
+  const rankInfo = await RankCtrl.getUserRankInfo(userId);
 
-  return res.json({
-    success: true,
+  if (rankInfo) {
+    const result = await this.create(userId, rankInfo, true);
+    if (result) {
+      res.json({
+        success: true,
+      });
+    }
+  }
+  res.json({
+    success: false,
   });
 });
+
+exports.create = async (userId, rankInfo, isClaimed) => {
+  if (!rankInfo.rank) return;
+  const rankId = rankInfo.rank._id;
+  const amount = rankInfo.amount;
+
+  const reward = new Reward({
+    userId,
+    rankId,
+    amount,
+    isClaimed,
+  });
+
+  return await reward.save();
+};
