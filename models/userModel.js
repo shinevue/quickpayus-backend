a6df3ad83fe66f75a58625d9d4fcfa33c64c6e7c
hy-hydra0 <hy.hydra@outlook.com>
2024-07-17 17:50:05 +0900

    fix: add role to  generating token

--- models/userModel.js
+++ models/userModel.js
@@ -192,7 +192,7 @@ userSchema.methods.compareField = async function (fieldData) {
   return this[key] === value;
 };
 userSchema.methods.getJWTToken = function () {
-  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+  return jwt.sign({ id: this._id, role: this.role }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE,
   });
 };
