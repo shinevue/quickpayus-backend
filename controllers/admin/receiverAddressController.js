741be8542eaed8e01765e58418c8b363b562b8f4
shinevue <shine401020@gmail.com>
2024-07-24 15:18:59 -0400

    fix: get request added

--- controllers/admin/receiverAddressController.js
+++ controllers/admin/receiverAddressController.js
@@ -2,8 +2,22 @@ const ErrorHandler = require("../../utils/errorHandler");
 const catchAsyncErrors = require("../../middlewares/catchAsyncErrors");
 
 const Receiver = require("../../models/receiverAddressModel");
-const User = require("../../models/userModel");
 
+exports.getAllReceiver = catchAsyncErrors(async (req, res, next) => {
+  const data = await Receiver.find({});
+
+  if (!data?.length) {
+    return res.json({
+      success: false,
+      data: [],
+    });
+  }
+
+  return res.json({
+    success: true,
+    data,
+  });
+});
 
 exports.addReceiver = catchAsyncErrors(async (req, res, next) => {
   const newReceiver = new Receiver({...req.body, adminId: req.user.username});
--- routes/admin/receiverAddressRoutes.js
+++ routes/admin/receiverAddressRoutes.js
@@ -4,11 +4,16 @@ const {
   authorizeRole,
 } = require("../../middlewares/auth");
 const {
+  getAllReceiver,
   addReceiver,
 } = require("../../controllers/admin/receiverAddressController");
 
 const router = express.Router();
 
-router.route("/").post(isAuthenticatedUser, authorizeRole, addReceiver);
+router
+  .route("/")
+  .get(isAuthenticatedUser, authorizeRole, getAllReceiver)
+  .post(isAuthenticatedUser, authorizeRole, addReceiver);
+
 
 module.exports = router;
