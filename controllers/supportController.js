2046cf57c96e6bc8c313f062648737afe246accc
byteslinger00 <gru.papoy.dev@gmail.com>
2024-08-01 08:33:10 -0400

    fix: update feedback controller

--- controllers/supportController.js
+++ controllers/supportController.js
@@ -4,14 +4,30 @@ const FeedBack = require("../models/feedbackModel");
 const Ticket = require("../models/ticketModel");
 
 exports.createFeedback = catchAsyncErrors(async (req, res, next) => {
-    const {id} = req.user;
-    const extension = req.file?.mimetype.split("/")[1];
-    const uploadedfilename = req.file?.path + "." + extension;
-    const feedback = {
-        userId: id,
-        fbCnt: req.body.feedback,
-        uploadedUrl: uploadedfilename
-    };
+    const { id } = req.user;
+    let feedback;
+    if(req.file) {
+
+        const oldPath = path.join(__dirname, '../uploads', req.file.filename);
+        const extension = req.file?.mimetype.split("/")[1];
+
+        // Rename the file
+        fs.renameSync(oldPath, `${oldPath}.${extension}`);
+
+        const uploadedfilename = `uploads/${req.file.filename}.${extension}`;
+        feedback = {
+            userId: id,
+            fbCnt: req.body.feedback,
+            uploadedUrl: uploadedfilename
+        };
+    }
+    else {
+        feedback = {
+            userId: id,
+            fbCnt: req.body.feedback,
+            uploadedUrl: ""
+        }
+    }
     const newFeedBack = new FeedBack(feedback);
     try{
         await newFeedBack.save();
