4d7a22610aa0b36358f28d38c7e4ca20d79c140f
hy-hydra0 <hy.hydra@outlook.com>
2024-08-01 18:39:35 +0900

    fix: set the otp middleware

--- routes/otpRoutes.js
+++ routes/otpRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const { isAuthenticatedUser } = require("../middlewares/auth");
 const otpCtlr = require("../controllers/otpController");
 const router = express.Router();
-
-router.route("/create").post(isAuthenticatedUser, otpCtlr.create);
-router.route("/verify").post(isAuthenticatedUser, otpCtlr.verify);
+const otpMiddleware = require("../middlewares/otp");
+router.route("/create").post(isAuthenticatedUser, otpMiddleware, otpCtlr.create);
+router.route("/verify").post(isAuthenticatedUser, otpMiddleware, otpCtlr.verify);
 router.route("/send").post(isAuthenticatedUser, otpCtlr.send);
 router.route("/confirm").post(otpCtlr.confirm);  // send otp code after sign up immediatley.
 
