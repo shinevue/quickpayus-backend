c599b3ed2febe3be0f5080f603bda555293f8f62
shinevue <shine401020@gmail.com>
2024-08-04 16:53:38 -0400

    fix: convert2ES index, app, update type in package and target in tsconfig

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const helmet = require("helmet"); // Security middleware
-const rateLimit = require("express-rate-limit"); // Rate limiting middleware
-const morgan = require("morgan");
-const path = require("path");
-const cors = require("cors");
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet'; // Security middleware
+import rateLimit from 'express-rate-limit'; // Rate limiting middleware
+import morgan from 'morgan';
+import path from 'path';
+import cors from 'cors';
+import errorMiddleware from './src/utils/errorHandler.ts';
+
 const app = express();
-const errorMiddleware = require("./src/middlewares/defaultError");
 
 const MAX_API_RATE_LIMIT = process.env.MAX_API_RATE_LIMIT || 3000000;
 
 const MAX_BAN_TIME_MS = process.env.MAX_BAN_TIME_MS || 1 * 60 * 1000;
 
-app.enable("trust proxy");
-app.set("trust proxy", 1);
+app.enable('trust proxy');
+app.set('trust proxy', 1);
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 const userRateLimiter = rateLimit({
   windowMs: MAX_BAN_TIME_MS,
@@ -35,42 +36,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
-    exposedHeaders: ["X-Total-Records", "X-Total-Pages"],
-  })
+    exposedHeaders: ['X-Total-Records', 'X-Total-Pages'],
+  }),
 );
 
 //route Imports
-const authRoutes = require("./src/routes/authRoutes");
-const adminUserRoutes = require("./src/routes/admin/usersRoutes.js");
-const profileRoutes = require("./src/routes/admin/profileRoutes");
-const adminRoleRoutes = require("./src/routes/admin/roleRoutes.js");
-const adminReceiverAddress = require("./src/routes/admin/receiverAddressRoutes.js");
-const adminTransactionRoutes = require("./src/routes/admin/transactionRoutes");
-const notificationRoutes = require("./src/routes/notificationRoutes");
-const programRoutes = require("./src/routes/programRoutes");
-const ranksRoutes = require("./src/routes/admin/ranksRoutes");
-const adminProfitConfigRoutes = require("./src/routes/admin/profitConfigRoutes");
-const transactionRoutes = require("./src/routes/transactionRoutes");
-const referralRoutes = require("./src/routes/referralRoutes");
-const userRoutes = require("./src/routes/userRoutes");
-const analyticsRoutes = require("./src/routes/analyticsRoutes");
-const adminAnalyticsRoutes = require("./src/routes/admin/analyticsRoutes");
-const adminAnnouncementRoutes = require("./src/routes/admin/announcementRoutes");
-const adminNotificationRoutes = require("./src/routes/admin/notificationRoutes.js");
-const announcementRoutes = require("./src/routes/announcementRoutes");
-const rankRoutes = require("./src/routes/rankRoutes");
-const otpRoutes = require("./src/routes/otpRoutes");
-const rewardRoutes = require("./src/routes/rewardRoutes");
-const supportRoutes = require("./src/routes/supportRoutes");
+import authRoutes from './src/routes/authRoutes';
+import adminUserRoutes from './src/routes/admin/usersRoutes.ts';
+import profileRoutes from './src/routes/admin/profileRoutes';
+import adminRoleRoutes from './src/routes/admin/roleRoutes.ts';
+import adminReceiverAddress from './src/routes/admin/receiverAddressRoutes.ts';
+import adminTransactionRoutes from './src/routes/admin/transactionRoutes';
+import notificationRoutes from './src/routes/notificationRoutes';
+import programRoutes from './src/routes/programRoutes';
+import ranksRoutes from './src/routes/admin/ranksRoutes';
+import adminProfitConfigRoutes from './src/routes/admin/profitConfigRoutes';
+import transactionRoutes from './src/routes/transactionRoutes';
+import referralRoutes from './src/routes/referralRoutes';
+import userRoutes from './src/routes/userRoutes';
+import analyticsRoutes from './src/routes/analyticsRoutes';
+import adminAnalyticsRoutes from './src/routes/admin/analyticsRoutes';
+import adminAnnouncementRoutes from './src/routes/admin/announcementRoutes';
+import adminNotificationRoutes from './src/routes/admin/notificationRoutes.ts';
+import announcementRoutes from './src/routes/announcementRoutes';
+import rankRoutes from './src/routes/rankRoutes';
+import otpRoutes from './src/routes/otpRoutes';
+import rewardRoutes from './src/routes/rewardRoutes';
+import supportRoutes from './src/routes/supportRoutes';
 
-// const {checkDeletedUser} = require("./src/controllers/authController");    // import check deleted user inspect part
+// import {checkDeletedUser} from "./src/controllers/authController";    // import check deleted user inspect path
 
-// const {seedDummyUsers} = require("./src/seeder/users");
+// import {seedDummyUsers} from "./src/seeder/users";
 
-const BASE_ROUTE = "/api/v1";
+const BASE_ROUTE = '/api/v1';
 
 app.use(`${BASE_ROUTE}/auth`, authRoutes);
 app.use(`${BASE_ROUTE}/user`, userRoutes);
@@ -101,20 +102,24 @@ app.use(`${BASE_ROUTE}/support`, supportRoutes);
 // Make seed user's profile data
 // seedDummyUsers();
 
-app.set("view engine", "ejs");
-app.set("views", "./utils/emailTemplates");
+app.set('view engine', 'ejs');
+app.set('views', './utils/emailTemplates');
 
-app.get("/email", async (req, res) => {
-  res.render("emailConfirm", {
-    title: "EJS Components Example",
-    content: "This is the content of the main page.",
+app.get('/email', async (req, res) => {
+  res.render('emailConfirm', {
+    title: 'EJS Components Example',
+    content: 'This is the content of the main page.',
   });
 });
 
-app.use('/uploads', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // or specify your origin
-  next();
-}, express.static(path.join(__dirname, 'uploads')));
+app.use(
+  '/uploads',
+  (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // or specify your origin
+    next();
+  },
+  express.static(path.join(__dirname, 'uploads')),
+);
 
 // errorMidelware
 app.use(errorMiddleware);
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const dotenv = require("dotenv");
-const app = require("./app.js");
+import dotenv from 'dotenv';
+import app from './app.js';
 
-const connectDB = require("./src/config/db.js");
+import connectDB from './src/config/db.js';
 //Handling uncaught exception
-process.on("uncaughtException", (err) => {
+process.on('uncaughtException', (err) => {
   console.log(`Error ${err.message}`);
   console.log(`Shutting down server due to Uncaught Rejection`);
   process.exit(1);
@@ -11,7 +11,7 @@ process.on("uncaughtException", (err) => {
 //config
 dotenv.config();
 
-const receiverAddressCtr = require("./src/controllers/admin/receiverAddressController")
+import receiverAddressCtr from './src/controllers/admin/receiverAddressController';
 receiverAddressCtr.defaultReceiver();
 
 const server = app.listen(process.env.PORT, async () => {
@@ -19,7 +19,7 @@ const server = app.listen(process.env.PORT, async () => {
 });
 connectDB();
 // unhandled Promise Rejection
-process.on("unhandledRejection", (err) => {
+process.on('unhandledRejection', (err) => {
   console.log(`Error ${err.message}`);
   console.log(`Shutting down server due to Unhandled Rejection`);
   server.close(() => {
