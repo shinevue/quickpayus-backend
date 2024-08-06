45fd76f89d4855089873c9b42bf4cee538ea6d2f
shinevue <shine401020@gmail.com>
2024-08-06 15:39:43 -0400

    fix: update model for every user's action

--- src/models/notificationModel.ts
+++ src/models/notificationModel.ts
@@ -1,14 +1,20 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema } from 'mongoose';
 
-// Define an interface for the Notification document
+interface ActionType {
+  username: string;
+  isDelete: boolean;
+  isRead: boolean;
+  update: Date;
+}
+
 interface INotification extends Document {
   userId: string;
   title: string;
   message: string;
-  isRead?: boolean;
   adminCreated?: boolean;
   type?: string;
   link?: string;
+  action?: ActionType[];
 }
 
 // Define the notification schema
@@ -17,16 +23,28 @@ const notificationSchema: Schema = new Schema(
     userId: { type: String, required: true, minlength: 1 },
     title: { type: String, required: true, minlength: 1 },
     message: { type: String, required: true, minlength: 1 },
-    isRead: { type: Boolean, default: false },
-    adminCreated: { type: Boolean, default: false },
-    type: { type: String, default: "GENERAL" }, // Corrected "GERNERAL" to "GENERAL"
+    type: { type: String, default: 'GENERAL' }, // Corrected "GERNERAL" to "GENERAL"
     link: { type: String },
+    adminCreated: { type: Boolean, default: false },
+    action: [
+      {
+        user: {
+          username: { type: String, default: '' },
+          isDelete: { type: Boolean, default: false },
+          isRead: { type: Boolean, default: false },
+          update: { type: Date, default: Date.now },
+        },
+      },
+    ],
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 // Create an index to automatically delete documents after 259200 seconds (3 days)
 notificationSchema.index({ userId: 1 }, { expireAfterSeconds: 259200 });
 
 // Export the Notification model
-export const Notification = mongoose.model<INotification>("Notification", notificationSchema);
+export const Notification = mongoose.model<INotification>(
+  'Notification',
+  notificationSchema,
+);
